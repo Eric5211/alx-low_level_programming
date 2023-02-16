39df18c3d15111f90e71ab3ea6c28c594301c4fd
Eric5211 <erickimani248@gmail.com>
2023-02-16 13:47:27 -0800

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
