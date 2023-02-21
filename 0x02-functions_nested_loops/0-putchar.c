a3724184b2cc81dbba374b36b79755cfa9d8ecc2
Eric5211 <erickimani248@gmail.com>
2023-02-21 12:02:21 -0800

    d

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+ #include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    /* Use _putchar to print "_putchar" and a newline */
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int putchar(int);
+void print_alphabet_x10(void);
+void print_alphabet(void);
+int _isalpha(int c);
+int _islower(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+
+#endif
