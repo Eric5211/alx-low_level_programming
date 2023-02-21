55ba4d4d27b2cddec57061078528a2e24f84a994
Eric5211 <erickimani248@gmail.com>
2023-02-21 14:56:58 -0800

    s

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - Return 1 if char is uppercase
+ * else, return 0
+ *
+ * @c: character in ASCII mode
+ *
+ * Return: 1 for uppercase,0 if otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
+	putchar('\n');
+}
