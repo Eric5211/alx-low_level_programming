f61a3b3cae06eaea3c42d20a922abcb885de3f92
Eric5211 <erickimani248@gmail.com>
2023-02-21 15:51:55 -0800

    d

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -18,5 +18,5 @@ int _isalpha(int c)
 	{
 	return (0);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -9,13 +9,13 @@ int main(void)
 {
     int r;
 	r = _isalpha('H');
-	putchar(r + '0');
+	_putchar(r + '0');
 	r = _isalpha('o');
-	putchar(r + '0');
+	_putchar(r + '0');
 	r = _isalpha(108);
-	putchar(r + '0');
+	_putchar(r + '0');
 	r = _isalpha(';');
-	putchar(r + '0');
-	putchar('\n');
+	_putchar(r + '0');
+	_putchar('\n');
     return (0);
 }
