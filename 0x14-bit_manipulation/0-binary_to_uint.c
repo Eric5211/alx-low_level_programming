fd5ae0981a4deba681f02c1e0c78d3b47d092254
Eric5211 <erickimani248@gmail.com>
2023-05-05 01:09:38 -0700

    added

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,27 +7,27 @@
  *
  * Return: unsigned int.
  */
-unsigned int binary_to_uint(const char *c)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int ui;
 	int len, base_two;
 
-	if (!c)
+	if (!b)
 		return (0);
 
 	ui = 0;
 
-	for (len = 0; c[len] != '\0'; len++)
+	for (len = 0; b[len] != '\0'; len++)
 		;
 
 	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (c[len] != '0' && c[len] != '1')
+		if (b[len] != '0' && b[len] != '1')
 		{
 			return (0);
 		}
 
-		if (c[len] & 1)
+		if (b[len] & 1)
 		{
 			ui += base_two;
 		}
